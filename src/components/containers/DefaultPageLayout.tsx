c153162b312261674ca515579ffaf00e4cfc07e7
xyashino <szczypiropan123@gmial.com>
2023-09-29 22:20:31 +0200

    Add DefaultPageLayout component
    This commit introduces a new DefaultPageLayout component. The component provides uniformity for all the pages in the application. It includes a header with a title and a 'go home' link, and a grid for page content. This design enhances user experience and eases navigation.

---
+++ src/components/containers/DefaultPageLayout.tsx
@@ -0,0 +1,35 @@
+import { PropsWithChildren } from "react";
+import { cn } from "@/lib/utils/cn";
+import { GoHomeLink } from "@/components/GoHomeLink";
+
+interface Props extends PropsWithChildren {
+  className?: string;
+  title?: string;
+  linkClassName?: string;
+}
+
+export const DefaultPageLayout = ({
+  children,
+  className,
+  title,
+  linkClassName,
+}: Props) => {
+  return (
+    <main
+      className={cn(
+        "w-full h-full p-2 lg:p-8 bg-black-light z-40 selection:bg-yellow selection:text-black overflow-y-scroll lg:overflow-hidden",
+        className,
+      )}
+    >
+      <header className="w-full p-2 flex border-b-2 border-b-current items-center justify-around sticky">
+        <h1 className="grow text-2xl lg:text-7xl uppercase  w-full mx-auto font-semibold font-mono italic break-words text-left ">
+          {title}
+        </h1>
+        <GoHomeLink className={linkClassName} />
+      </header>
+      <div className="grid place-items-center justify-between lg:grid-cols-2">
+        {children}
+      </div>
+    </main>
+  );
+};
