0380c996e5c419d96022a6fb3f10d4b784bca055
xyashino <szczypiropan123@gmial.com>
2023-09-29 22:36:30 +0200

    Add routes configuration file
    This commit introduces a new 'routes.tsx' file in the 'src/config' directory. This file is designed to manage the navigation menu links and related settings on the website. Each link is assigned with its corresponding navigational route, text, icon, theme and a className for CSS styling. This centralized configuration would simplify the management of navigation menu links across the application.

---
+++ src/config/routes.tsx
@@ -0,0 +1,37 @@
+import { NavigationItemProps } from "@/components/NavigationSection/navigationItem";
+import { Navigation } from "@/types/enum/navigation";
+import { Project } from "@/components/icons/Project";
+import { Contact } from "@/components/icons/Contact";
+import { Question } from "@/components/icons/Question";
+import { Stack } from "@/components/icons/Stack";
+
+export const ROUTES: NavigationItemProps[] = [
+  {
+    to: Navigation.Projects,
+    className: "lg:row-span-2 col-span-3 lg:col-span-1 bg-white text-accent",
+    text: "Projects",
+    icon: <Project />,
+    theme: "white",
+  },
+  {
+    to: Navigation.Contact,
+    className: "col-span-2 bg-yellow text-black ",
+    text: "Hire Me",
+    icon: <Contact />,
+    theme: "yellow",
+  },
+  {
+    to: Navigation.About,
+    className: "bg-blue text-white",
+    text: "About Me",
+    icon: <Question />,
+    theme: "blue",
+  },
+  {
+    to: Navigation.Stack,
+    className: "col-span-3 lg:col-span-1 bg-accent text-white",
+    text: "My Stack",
+    icon: <Stack />,
+    theme: "black",
+  },
+];
