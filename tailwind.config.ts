a2f4036a47912dea1f9978ecbdf19ea24aab00aa
xyashino <szczypiropan123@gmial.com>
2023-09-29 22:17:55 +0200

    Update Tailwind CSS config with new theme variables
    The configuration for Tailwind CSS was updated to include new theme variables. Specifically, variables for scroll text animations, keyframe animations, and new color schemes were added. This will allow for more consistent and flexible styling across the application, and enable smoother text scrolling effects. Control of animations and color variables directly from the config file will streamline the design process.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        scrollText: "scrollText 21s linear infinite",
+      },
+      keyframes: {
+        scrollText: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-50%)" },
+        },
+      },
+      colors: {
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          content: "hsl(var(--primary-content))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          content: "hsl(var(--secondary-content))",
+        },
+        accent: "hsl(var(--accent))",
+        yellow: "hsl(var(--yellow))",
+        blue: "hsl(var(--blue))",
+        black: "hsl(var(--black))",
+        white: "hsl(var(--white))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
