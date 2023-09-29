92a6398db780b2a77976d855aa8f7eda544b95f9
xyashino <szczypiropan123@gmial.com>
2023-09-29 22:19:49 +0200

    Add new SVG icons for better UI experience
    These commit includes the addition of new SVG icons (Project, Question, Stack, Home and Contact). The new SVGs have been added to improve the user interface, providing more intuitive navigation for users. The icons will represent different sections of the application, making it easier for users to understand the functionality of each section.

---
+++ src/components/icons/Home.tsx
@@ -0,0 +1,13 @@
+import { SVGProps } from "react";
+
+export const Home = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path fill="currentColor" d="M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5z"></path>
+  </svg>
+);
