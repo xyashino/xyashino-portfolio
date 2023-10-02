65798e83e66b857ea98d4462cc51053c80f6b465
xyashino <szczypiropan123@gmial.com>
2023-10-02 11:53:57 +0200

    Refactor Dialog component for improved UX
    In this commit, the code base for the Dialog component was refactored. The primary reason for this change was to improve user experience.
    
    Previously, we were using a simple Transition component for dialog transitions. Now, using the AnimatePresence and motion modules from the framer-motion library, we have made the animation more lively and engaging and so, enhancing the UX.
    
    We also added escape key functionality to close the dialog to improve accessibility. We also started using useRef hook to ensure that the focus is kept within the dialog when it's open, again, a vital part of ensuring our applications accessibility.

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,12 +1,7 @@
 "use client";
-import { Fragment, PropsWithChildren } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { cn } from "@/lib/utils/cn";
-import { className } from "postcss-selector-parser";
-import {
-  MODAL_BACKGROUND_TRANSITION,
-  MODAL_CONTENT_TRANSITION,
-} from "@/config/constants/transitions";
+import { PropsWithChildren, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useFocusTrap } from "@/lib/hooks/useFocusTrap";
 
 interface Props extends PropsWithChildren {
   isOpen: boolean;
@@ -15,30 +10,50 @@ interface Props extends PropsWithChildren {
 }
 
 export const AnimatedDialog = ({ children, isOpen, close }: Props) => {
+  const focusRef = useFocusTrap();
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") close();
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, close]);
+
   return (
-    <>
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog onClose={close} className="relative z-30" as="div">
-          {/*Backdrop*/}
-          <Transition.Child as={Fragment} {...MODAL_BACKGROUND_TRANSITION}>
-            <div className="fixed inset-0 bg-secondary/80" />
-          </Transition.Child>
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child as={Fragment} {...MODAL_CONTENT_TRANSITION}>
-                <Dialog.Panel
-                  className={cn(
-                    "bg-accent w-full max-w-2xl transform overflow-hidden  p-8 text-left align-middle drop-shadow-lg shadow-black transition-all text-white selection:bg-yellow selection:text-black space-y-4",
-                    className,
-                  )}
-                >
-                  {children}
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
-    </>
+    <div ref={focusRef}>
+      <AnimatePresence initial={isOpen}>
+        {isOpen && (
+          <motion.div
+            key="modal"
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            transition={{
+              ease: "easeInOut",
+              duration: 0.3,
+            }}
+            exit={{ x: "100%" }}
+            className="fixed inset-0 flex justify-center items-center bg-secondary/80 z-50"
+            onClick={close}
+          >
+            <motion.div
+              className="bg-accent w-full max-w-2xl transform overflow-hidden  p-8 text-left align-middle drop-shadow-lg shadow-black transition-all text-white selection:bg-yellow selection:text-black space-y-4"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                ease: "easeInOut",
+                duration: 0.2,
+                delay: 0.2,
+              }}
+              onClick={(e) => e.stopPropagation()}
+              tabIndex={0}
+            >
+              {children}
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 };
