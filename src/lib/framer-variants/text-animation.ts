34792c762014bf0d84461c183206a33bdf0ec553
xyashino <szczypiropan123@gmial.com>
2023-10-03 15:22:49 +0200

    Add text animation variants for framer-motion
    A new file, text-animation.ts, has been added to the src/lib/framer-variants directory. This file introduces two animation variants, textContainerVariant and wordVariant, for use with framer-motion's staggered animation functionality. These will provide a smoother, spring-damping animation to the text elements within the application.

---
+++ src/lib/framer-variants/text-animation.ts
@@ -0,0 +1,33 @@
+import { Variants } from "framer-motion";
+
+export const textContainerVariant: Variants = {
+  hidden: { opacity: 0 },
+  visible: (i = 1) => ({
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.12,
+      delayChildren: i * 0.1,
+    },
+  }),
+};
+
+export const wordVariant: Variants = {
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      damping: 12,
+      stiffness: 100,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    x: 20,
+    transition: {
+      type: "spring",
+      damping: 12,
+      stiffness: 100,
+    },
+  },
+} as Variants;
