fbbe323535aa6298772149ca2ad3e9c562b8b83b
xyashino <szczypiropan123@gmial.com>
2023-10-04 15:17:30 +0200

    Update Projects page with dynamic project data
    Included functionality to render a list of dynamically loaded project cards in the Projects page. This was done by importing a getProjectsData function and PROJECTS_DIR_PATH constant from lib and config folders respectively. The Project card data is fetched asynchronously in the Projects function, and then mapped to ProjectCard components that are displayed in a flex container. This change is intended to enhance the scalability and maintenance efficiency of the Projects page by allowing easy addition and modification of project cards.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,30 @@
 import { DefaultPageLayout } from "@/components/containers/DefaultPageLayout";
+import { React } from "@/components/icons/stack";
+import { ProjectCard } from "@/components/ProjectCard";
+import { getProjectsData } from "@/lib/mdx";
+import { PROJECTS_DIR_PATH } from "@/config/constants/md";
 
-export default function Projects() {
+export default async function Projects() {
+  const projects = await getProjectsData<ProjectMetadata>(PROJECTS_DIR_PATH);
+
   return (
     <DefaultPageLayout
       title="Projects"
-      className="text-black bg-white"
+      className="text-black bg-white lg:overflow-y-scroll no-scrollbar"
       linkClassName="hover:bg-black hover:text-white border-black text-black"
-    ></DefaultPageLayout>
+      headerClassName="bg-white"
+      containerClassName="container mx-auto w-full h-full text-black"
+    >
+      <div className="flex flex-col  space-y-12 pb-12">
+        {projects.map(({ data, fileName }, i) => (
+          <ProjectCard
+            key={`${fileName}-${i}`}
+            {...data}
+            fileName={fileName}
+            reverse={!!(i % 2)}
+          />
+        ))}
+      </div>
+    </DefaultPageLayout>
   );
 }
