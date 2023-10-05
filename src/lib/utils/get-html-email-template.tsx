78419f0f71c0ceceabbde8ae29ea6940c70f71a6
xyashino <szczypiropan123@gmial.com>
2023-10-05 16:38:47 +0200

    Add email handling utility and example environmental file
    This commit introduces an HTML email template generation utility function in 'get-html-email-template.tsx' and a '.env.example' file. The utility function helps to generate well-structured HTML emails and the '.env.example' provides a template for necessary environment variables. This change intends to enhance manageability and readability in email communication.

---
+++ src/lib/utils/get-html-email-template.tsx
@@ -0,0 +1,58 @@
+import { ValidateForm } from "@/lib/utils/validate-form";
+
+export const getHtmlEmailTemplate = ({
+  email,
+  message,
+  name,
+}: ReturnType<typeof ValidateForm>) => {
+  return `
+    <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Nowa wiadomość z formularza kontaktowego</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 18px;
+        }
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 10px;
+            border: 1px solid #e0e0e0;
+            background-color: blue;
+        }
+        .header {
+            text-align: center;
+            color: white;
+            text-transform: uppercase;
+            margin-bottom: 20px;
+        }
+        .message-content {
+            padding: 20px;
+            background-color: #ffffff;
+            border: 1px solid #e0e0e0;
+        }
+        h2 {
+            font-size: 24px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h2>Nowa wiadomość z formularza kontaktowego</h2>
+        </div>
+        <div class="message-content">
+            <p><strong>Imię:</strong> ${name}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <p><strong>Wiadomość:</strong></p>
+            <p>${message}</p>
+        </div>
+    </div>
+</body>
+</html>
+    
+    `;
+};
