cbd66308844f0f8565808dc5b3bc31fcae6c208a
xyashino <szczypiropan123@gmial.com>
2023-10-05 16:37:58 +0200

    Replace AnimatedDialog with Dialog in InfoSection
    The AnimatedDialog is replaced with Dialog to improve performance. Additionally, a custom class for background color and the new DomAnimation component is introduced. This change improves the code readability, offering a smoother transition without affecting the overall functionality of the application.

--- src/components/InfoSection/resumeDialog/index.tsx
+++ src/components/InfoSection/resumeDialog/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { AnimatedDialog } from "@/components/Dialog";
+import { Dialog } from "@/components/Dialog";
 import { useDialog } from "@/lib/hooks/useDialog";
 import { DownloadSection } from "./downloadSection";
 import { Button } from "@/components/Button";
@@ -16,12 +16,12 @@ export const ResumeDialog = () => {
       >
         Get my Resume
       </Button>
-      <AnimatedDialog isOpen={isOpen} close={handleClose}>
+      <Dialog isOpen={isOpen} close={handleClose}>
         <h3 className="text-xl lg:text-4xl font-extrabold font-mono leading-6  uppercase italic border-b-2 pb-2">
           Get my Resume
         </h3>
         <DownloadSection />
-      </AnimatedDialog>
+      </Dialog>
     </>
   );
 };
