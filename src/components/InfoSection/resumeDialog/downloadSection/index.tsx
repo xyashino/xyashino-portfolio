02a19c21d95f4789eef721e15da4055bb9db3616
xyashino <szczypiropan123@gmial.com>
2023-10-05 10:35:23 +0200

    Remove DownloadResumeBtn, update text size & fix index.tsx
    Removed the `DownloadResumeBtn` component as it was no longer required. The resume is now directly accessible through a URL, instead of being downloaded. This change was made to improve user experience and flexibility.
    
    Modified the font size for `highlightedText.tsx` for better readability on different screen sizes.
    
    Refactored `index.tsx` to handle resume URL switch based on selected language. Updated layout for a more streamlined and centered visual presentation.
    
    Added `resume.ts` constants file containing URLs for different language resumes.

--- src/components/InfoSection/resumeDialog/downloadSection/index.tsx
+++ src/components/InfoSection/resumeDialog/downloadSection/index.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import React, { useState } from "react";
 import { HighlightedText } from "./highlightedText";
 import { LanguageSwitch } from "./languageSwitch";
-import { DowloadResumeBtn } from "./dowloadResumeBtn";
+import { ENG_RESUME_URL, PL_RESUME_URL } from "@/config/constants/resume";
 
 export const DownloadSection = () => {
   const [isEnglish, setIsEnglish] = useState(true);
   return (
-    <div>
+    <div className="flex-col flex items-center justify-center space-y-2 lg:space-y-4 m-2">
       <p className="text-2xl lg:text-3xl font-extrabold uppercase italic font-mono text-center">
         Select language of resume
       </p>
@@ -15,20 +16,14 @@ export const DownloadSection = () => {
         <LanguageSwitch isEnglish={isEnglish} setIsEnglish={setIsEnglish} />
         <HighlightedText highlight={isEnglish} text="English 🇬🇧" />
       </div>
-      <section>
-        <div className="flex justify-between space-x-8 py-2">
-          <DowloadResumeBtn
-            text="Download"
-            href="/resume.pdf"
-            download="resume.pdf"
-          />
-          <DowloadResumeBtn
-            text="Online"
-            href="/resume.pdf"
-            download="resume.pdf"
-          />
-        </div>
-      </section>
+      <Link
+        href={isEnglish ? ENG_RESUME_URL : PL_RESUME_URL}
+        rel="noopener noreferrer"
+        target="_blank"
+        className="block cursor-pointer py-2 px-8 lg:text-xl italic uppercase font-bold border-2 border-2-white text-center hover:bg-secondary hover:text-secondary-content transition-colors duration-300 ease-in-out hover:border-secondary"
+      >
+        Click to see my resume
+      </Link>
     </div>
   );
 };
---
+++ src/config/constants/resume.ts
@@ -0,0 +1,4 @@
+export const ENG_RESUME_URL =
+  "https://drive.google.com/file/d/1DLkpM7vD8Br1SxDMaqTr8UZV0l9re7uB/view?usp=sharing";
+export const PL_RESUME_URL =
+  "https://drive.google.com/file/d/1DLkpM7vD8Br1SxDMaqTr8UZV0l9re7uB/view?usp=sharing";
