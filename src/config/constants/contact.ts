9b64b4436acb6d074b7661913f3fa9a6f2e3924e
xyashino <szczypiropan123@gmial.com>
2023-10-06 20:38:13 +0200

    Remove phone contact information from constants
    Phone contact option was removed from the contact constants due to privacy reasons. We want to limit the channels through which users can reach out to us, as it's more manageable and efficient for our team. Now, users can contact us through email and LinkedIn.

--- src/config/constants/contact.ts
+++ src/config/constants/contact.ts
@@ -1,16 +1,10 @@
-const { NEXT_PUBLIC_EMAIL, NEXT_PUBLIC_PHONE, NEXT_PUBLIC_LINKEDIN } =
-  process.env;
+const { NEXT_PUBLIC_EMAIL, NEXT_PUBLIC_LINKEDIN } = process.env;
 
 export const CONTACT_DATA = [
   {
     title: "By email:",
     link: `mailto:${NEXT_PUBLIC_EMAIL}`,
     text: NEXT_PUBLIC_EMAIL,
-  },
-  {
-    title: "By phone:",
-    link: `tel:${NEXT_PUBLIC_PHONE}`,
-    text: NEXT_PUBLIC_PHONE,
   },
   {
     title: "By LinkedIn:",
