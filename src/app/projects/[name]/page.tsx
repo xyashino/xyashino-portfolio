551c6ad3016f8e603e423b941c4d970874f852ec
xyashino <szczypiropan123@gmial.com>
2023-10-07 16:32:44 +0200

    Remove PageWrapper from various pages
    PageWrapper component was removed from projects, stack, individual project, about and contact pages. This was done to simplify the structure and reduce unnecessary code. All functionalities previously handled by the PageWrapper are now managed directly within the DefaultPageLayout component which leads to a more clear and readable codebase.

--- src/app/projects/[name]/page.tsx
+++ src/app/projects/[name]/page.tsx
@@ -6,7 +6,6 @@ import { Arrow } from "@/components/icons/Arrow";
 import { YoutubeIframe } from "@/components/YoutubeIframe";
 import { TagList } from "@/components/ProjectCard/TagList";
 import { Navigation } from "@/types/enum/navigation";
-import { PageWrapper } from "@/components/containers/PageWrapper";
 import { ContrastCard } from "@/components/ContrastCard";
 
 type ParamsWithName = {
@@ -37,32 +36,30 @@ export default async function ArticlePage({
   const { content, data } = article;
 
   return (
-    <PageWrapper className="bg-white">
-      <div className="w-full h-full relative overflow-y-scroll no-scrollbar bg-white selection:bg-yellow selection:text-accent">
-        <header className="w-full p-4 flex border-b-2 border-b-current items-center z-30 top-0 bg-white sticky">
-          <Link
-            href={Navigation.Projects}
-            className="hover:bg-black hover:text-white border-black text-black text-4xl lg:text-7xl transition-colors duration-300 rounded-full"
-          >
-            <Arrow className="rotate-180" />
-          </Link>
-          <h1 className="ml-auto text-2xl lg:text-7xl uppercase  mx-auto font-extrabold font-mono italic break-words text-left">
-            {data.title}
-          </h1>
-        </header>
-        <section className="container mx-auto mb-16">
-          <div className="flex flex-col items-center">
-            <YoutubeIframe embedId={data.embedId} className="w-full lg:w-3/4" />
-            <ContrastCard className="w-full lg:w-3/4">
-              {data.description}
-            </ContrastCard>
-            <TagList tags={data.tags} />
-          </div>
-          <article className=" p-2 prose lg:prose-xl mx-auto prose-a:text-blue prose-code:font-extrabold mt-4">
-            <MDXRemote source={content} />
-          </article>
-        </section>
-      </div>
-    </PageWrapper>
+    <div className="w-full h-full relative overflow-y-scroll no-scrollbar bg-white selection:bg-yellow selection:text-accent">
+      <header className="w-full p-4 flex border-b-2 border-b-current items-center z-30 top-0 bg-white sticky">
+        <Link
+          href={Navigation.Projects}
+          className="hover:bg-black hover:text-white border-black text-black text-4xl lg:text-7xl transition-colors duration-300 rounded-full"
+        >
+          <Arrow className="rotate-180" />
+        </Link>
+        <h1 className="ml-auto text-2xl lg:text-7xl uppercase  mx-auto font-extrabold font-mono italic break-words text-left">
+          {data.title}
+        </h1>
+      </header>
+      <section className="container mx-auto mb-16">
+        <div className="flex flex-col items-center">
+          <YoutubeIframe embedId={data.embedId} className="w-full lg:w-3/4" />
+          <ContrastCard className="w-full lg:w-3/4">
+            {data.description}
+          </ContrastCard>
+          <TagList tags={data.tags} />
+        </div>
+        <article className=" p-2 prose lg:prose-xl mx-auto prose-a:text-blue prose-code:font-extrabold mt-4">
+          <MDXRemote source={content} />
+        </article>
+      </section>
+    </div>
   );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,4 @@
 import { DefaultPageLayout } from "@/components/containers/DefaultPageLayout";
-import { PageWrapper } from "@/components/containers/PageWrapper";
 import { ProjectCard } from "@/components/ProjectCard";
 import { getProjectsData } from "@/lib/mdx";
 
@@ -10,26 +9,24 @@ export default async function Projects() {
   const projects = await getProjectsData<ProjectMetadata>(PROJECTS_DIR_PATH);
 
   return (
-    <PageWrapper className="bg-white">
-      <DefaultPageLayout
-        title="Projects"
-        className="text-black bg-white lg:overflow-y-scroll no-scrollbar"
-        linkClassName="hover:bg-black hover:text-white border-black text-black"
-        headerClassName="bg-white"
-        containerClassName="container mx-auto w-full h-full text-black"
-      >
-        <div className="flex flex-col space-y-12 pb-12">
-          {projects.map(({ data, fileName }, i) => (
-            <ProjectCard
-              key={`${fileName}-${i}`}
-              {...data}
-              fileName={fileName}
-              reverse={!!(i % 2)}
-              index={i}
-            />
-          ))}
-        </div>
-      </DefaultPageLayout>
-    </PageWrapper>
+    <DefaultPageLayout
+      title="Projects"
+      className="text-black bg-white lg:overflow-y-scroll no-scrollbar"
+      linkClassName="hover:bg-black hover:text-white border-black text-black"
+      headerClassName="bg-white"
+      containerClassName="container mx-auto w-full h-full text-black"
+    >
+      <div className="flex flex-col space-y-12 pb-12">
+        {projects.map(({ data, fileName }, i) => (
+          <ProjectCard
+            key={`${fileName}-${i}`}
+            {...data}
+            fileName={fileName}
+            reverse={!!(i % 2)}
+            index={i}
+          />
+        ))}
+      </div>
+    </DefaultPageLayout>
   );
 }
