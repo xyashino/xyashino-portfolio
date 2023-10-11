3431f0b7e1e7e7fcf1da9256d75559b71bc05783
xyashino <szczypiropan123@gmial.com>
2023-10-11 15:05:59 +0200

    Replace control Input with uncontrolled one
    Modified Input component in src/components/contact/ContactForm/Input.tsx file to turn controlled input into an uncontrolled one. The 'value' and 'onChange' props were removed and replaced by 'defaultValue'. This change simplifies the code and improves performance by relieving React from tracking input's state and re-rendering on every change.

--- src/components/contact/ContactForm/Input.tsx
+++ src/components/contact/ContactForm/Input.tsx
@@ -1,9 +1,8 @@
-import React, { ChangeEvent, HTMLProps } from "react";
+import React, { HTMLProps } from "react";
 
 interface Props extends HTMLProps<HTMLInputElement>, DataTypeAttribute {
   label: string;
-  value: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  defaultValue: string;
 }
 
 export const Input = ({ label, value, onChange, ...props }: Props) => {
@@ -18,8 +17,6 @@ export const Input = ({ label, value, onChange, ...props }: Props) => {
       <div className="bg-black relative">
         <input
           {...props}
-          value={value}
-          onChange={onChange}
           className="lg:text-xl bg-black text-white px-3 lg:px-6 py-1 lg:py-2 border-white border-2 m-1 font-mono placeholder:text-white/70 placeholder:italic selection:text-black selection:bg-yellow focus:outline-none focus:border-yellow "
         />
       </div>
