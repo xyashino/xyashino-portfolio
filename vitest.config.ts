465b86d82aae4dc43be5a84e5f8d1e1015cce759
xyashino <szczypiropan123@gmial.com>
2023-10-11 19:02:28 +0200

    Add Vitest and Jest testing setup
    Added a vitest configuration file 'vitest.config.ts' setting up plugins, test environment and test setup files. Also, created 'setup.ts' in tests directory for handling cleanup, integration with Jest matchers and mocking browser features. This improves testing efficiency and reliability.

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+/// <reference types="@testing-library/jest-dom" />
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+  },
+});
