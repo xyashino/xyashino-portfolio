0980288a8ce06790a34c70f7a810908ebd033863
Szczepan Micek <micek.szczepan.w@gmail.com>
2025-03-30 22:52:50 +0200

    fix: Adjust scroll animation duration and enhance text duplication in InfiniteScrollText component

--- src/components/infinite-scroll-text.tsx
+++ src/components/infinite-scroll-text.tsx
@@ -1,16 +1,24 @@
 export const InfiniteScrollText = ({ text }: { text: string }) => {
   return (
-    <div className="absolute inset-0 flex z-0 select-none w-screen">
+    <div className="absolute inset-0 flex z-0 select-none w-screen overflow-hidden">
       <div className="flex whitespace-nowrap animate-scroll-text">
-        {Array.from({ length: 3 }).map((_, i) => (
+        {Array.from({ length: 6 }).map((_, i) => (
           <p
-            key={i}
-            className="text-[10rem] lg:text-[20rem] italic font-semibold w-1/2"
+            key={`first-${i}`}
+            className="text-[10rem] lg:text-[20rem] italic font-semibold"
+          >
+            {text} &nbsp;
+          </p>
+        ))}
+        {Array.from({ length: 6 }).map((_, i) => (
+          <p
+            key={`second-${i}`}
+            className="text-[10rem] lg:text-[20rem] italic font-semibold"
           >
             {text} &nbsp;
           </p>
         ))}
       </div>
     </div>
-  )
+  );
 }
