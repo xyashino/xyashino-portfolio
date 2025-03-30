00089cb360d2b45679e551e7884ed588caf51ff2
Szczepan Micek <micek.szczepan.w@gmail.com>
2025-03-30 22:25:22 +0200

    chore: Update test command and add Vite and Vitest configuration files

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,14 +1,13 @@
 /// <reference types="vitest" />
 /// <reference types="@testing-library/jest-dom" />
-import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import {defineConfig} from "vitest/config";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
     environment: "jsdom",
-    setupFiles: "./tests/setup.ts",
+    setupFiles: "./src/tests/setup.ts",
   },
 });
